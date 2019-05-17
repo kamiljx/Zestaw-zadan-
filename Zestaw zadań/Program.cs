1ad2d3082ed53809274e7d9fbbe6fc0ed907f00b
Kamil Rzeszutek <kamil.rzeszutek@microsoft.wsei.edu.pl>
2019-05-17 17:12:42 +0200

    first 2

--- Zestaw zadań/Program.cs
+++ Zestaw zadań/Program.cs
@@ -1,21 +1,24 @@
 ﻿using System;
+using System.Runtime.Remoting;
 
 namespace Zestaw_zadań
 {
-    internal class Program
+    internal static class Program
     {
         public static void Main(string[] args)
         {
-            int[] tab = {23, 57, 54, 745, 1, 3, 74, 3};
+            int[] tab = {23, 57, 54, 745,  3, 74, 3, 99, 101};
             wlasciwosci(tab);
         }
 
         public static void wlasciwosci(int[] tab)
         {
             Console.WriteLine(GetMaxValue(tab));
+            Console.WriteLine(GetMinValue(tab));
+            Console.WriteLine(MaxParityIndex(tab));
         }
 
-        public static int GetMaxValue(int[] tab)
+        private static int GetMaxValue(int[] tab)
         {
             int max = tab[0];
             for (int i = 0; i < tab.Length ; i++)
@@ -23,5 +26,25 @@ namespace Zestaw_zadań
                     max = tab[i];
             return max;
         }
+
+        public static int GetMinValue(int[] tab)
+        {
+            int min = tab[0];
+            for (int i = 0; i < tab.Length; i++)
+                if (min > tab[i])
+                    min = tab[i];
+            return min;
+        }
+
+        public static int MaxParityIndex(int[] tab)
+        {
+            int maxParity = tab[0];
+            for (int i = 2; i < tab.Length; i+= 2)
+                if (maxParity < tab[i])
+                    maxParity = tab[i];
+            return maxParity;
+
+        }
+        
     }
 }
